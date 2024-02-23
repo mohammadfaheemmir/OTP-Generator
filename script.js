6e9c94cef5c57800eb7f06b2bd32fcf4b3d46a8d
Faheem <138996989+mohammadfaheemmir@users.noreply.github.com>
2024-02-23 21:55:25 +0530

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+let generateOTP = () => {
+    
+    const otpLength = 6;
+
+    const otp = Math.floor(100000 + Math.random() * 900000);
+
+    document.getElementById("otpDisplay").innerText = `${otp}`;
+};
+
+document.getElementById("generateBtn").addEventListener("click", generateOTP);
+window.addEventListener("load", generateOTP);
